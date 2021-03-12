0fc27252df3f61ef5c65ede2d20fae86702f2907
Tanmay Negi <u6741351@anu.edu.au>
2021-03-12 14:34:58 +1100

    docstring updated

--- math_functions.py
+++ math_functions.py
@@ -1,6 +1,6 @@
 """ File name:   math_functions.py
-    Author:      <your name goes here>
-    Date:        <the date goes here>
+    Author:      Tanmay Negi
+    Date:        12/03/2021
     Description: This file defines a set of variables and simple functions.
 
                  It should be implemented for Exercise 1 of Assignment 0.
