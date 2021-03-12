8ece8aba61c734e26c1c055c16b8400686b1faeb
Tanmay Negi <u6741351@anu.edu.au>
2021-03-12 15:39:53 +1100

    --

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 """ File name:   hello_world.py
-    Author:
-    Date:
+    Author:      kuro
+    Date:        12/03/2021
     Description: This file prints hello world and a few other things to screen.
 """
 
-print("Hello World!")
+print("Hello Analogs")
 
 x = 2.718
 print("The", x, "th root of", x, "is:", x ** (1 / x))
